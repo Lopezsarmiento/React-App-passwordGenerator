4bd040a6d004acf4c044f272d6b9dd760cd956a2
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2020-06-12 16:17:01 -0300

    replace input selection for select tag

--- src/components/length.jsx
+++ src/components/length.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 
 function Length(props) {
-  const { onHandleLength, passlength } = props;
+  const { onHandleLength } = props;
+  const selectValues = [
+    { id: 1, value: 8 },
+    { id: 2, value: 9 },
+    { id: 3, value: 10 },
+    { id: 4, value: 11 },
+    { id: 5, value: 12 },
+    { id: 6, value: 13 },
+    { id: 7, value: 14 },
+    { id: 8, value: 15 },
+    { id: 9, value: 16 },
+  ];
   return (
     <div className="form-group row">
       <label htmlFor="pasword" className="col-sm-9 col-form-label">
-        Password Length <small>(from 8 to 16)</small>
+        Select Password length
       </label>
       <div className="col-sm-3">
-        <input
-          type="number"
+        <select
           className="form-control"
-          placeholder={passlength}
           aria-label=""
           id="password"
           name="password"
-          min="8"
-          max="16"
           onChange={onHandleLength}
           autoFocus
-        ></input>
+        >
+          {selectValues.map((val) => (
+            <option key={val.id} val={val.value}>
+              {val.value}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
